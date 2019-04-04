857d8a64dfeda6159087970f88cb5875e2d57b32
Hardspyne <79159707922@yandex.ru>
2019-04-04 16:21:59 +0300

    write Shortener and StorageStrategy interface

--- src/main/java/com/strategy/StorageStrategy.java
+++ src/main/java/com/strategy/StorageStrategy.java
@@ -1,4 +1,13 @@
 package com.strategy;
 
 public interface StorageStrategy {
+    boolean containsKey(Long key);
+
+    boolean containsValue(String value);
+
+    void put(Long key, String value);
+
+    Long getKey(String value);
+
+    String getValue(Long key);
 }
